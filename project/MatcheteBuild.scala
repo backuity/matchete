331965c50c344749becaa66fefd8aba628582582
Bruno Bieth <biethb@gmail.com>
2013-11-12 08:04:47 +0100

    added sonatype release details

--- project/MatcheteBuild.scala
+++ project/MatcheteBuild.scala
@@ -10,6 +10,9 @@ object MatcheteBuild extends Build {
       organization := "org.backuity",
       scalaVersion := "2.10.2",
 
+      homepage := Some(url("https://github.com/backuity/matchete")),
+      licenses := Seq("Apache 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0.html")),
+
       scalacOptions ++= Seq("-deprecation", "-unchecked"),
 
       libraryDependencies ++= Seq(
@@ -17,6 +20,29 @@ object MatcheteBuild extends Build {
         "junit"                 % "junit"                 % "4.10"      % "optional"),
 
       publishMavenStyle := true,
+
+      publishTo := {
+        val nexus = "https://oss.sonatype.org/"
+        if (version.value.trim.endsWith("SNAPSHOT"))
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+      },
+
+      pomIncludeRepository := { _ => false },
+
+      pomExtra :=
+        <scm>
+          <url>git@github.com:backuity/matchete.git</url>
+          <connection>scm:git:git@github.com:backuity/matchete.git</connection>
+        </scm>
+        <developers>
+          <developer>
+            <id>backuitist</id>
+            <name>Bruno Bieth</name>
+            <url>https://github.com/backuitist</url>
+          </developer>
+        </developers>,
 
       // do not publish documentation -- source is enough
       publishArtifact in packageDoc := false
